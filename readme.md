10340fcf2dc194d704a84b620a33d778f785a62f
Nakize <nakize@proton.me>
2024-01-15 12:51:29 +0100

    Zmiana Autobusu

--- readme.md
+++ readme.md
@@ -10,10 +10,21 @@
 > npm run start
 ```
 
-Now it should work on http://localhost:2137 (or other if you changed .env file. Otherwise is ::3000 port)
+# .ENV file
+add .env file. Env file should look like this:
+```env
+PORT=2137
+```
+
+# Routes
+
+Now it should work on http://localhost:2137 (or other if you changed .env file. Without file default port is ::3000)
 
 main page is on http://localhost:2137/ <- Here you can upload your files
 
-other page is under http://localhost:2137/fetch <- Here you can find uploaded files
+second page is under http://localhost:2137/fetch <- Here you can find uploaded files
+
+
+# Credits
 
 > Created by [Nakize](https://github.com/Yix0n)
--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,53 @@ import express, { Request, Response } from 'express';
 import { createServer } from 'http';
 import { config } from 'dotenv';
 import { join, extname } from 'path';
-import { readdirSync, readFile, existsSync, mkdirSync, writeFile, rename } from 'fs';
+import { readdirSync, readFile, existsSync, mkdirSync, writeFile, rename, createReadStream } from 'fs';
 import { IncomingForm, File } from 'formidable';
+import consolidate from 'consolidate';
 
 config();
+
+const POPECODE = 2137
 
 const app = express();
 const httpServer = createServer(app);
 const uploadDir = join(__dirname + '/uploads');
 
+// handlebar engine
+
+app.engine('hbs', consolidate.handlebars)
+app.set('view engine', 'hbs')
+app.set('views', join(process.cwd(), "src/hbs"))
+
 if (!existsSync(uploadDir)) mkdirSync(uploadDir, '0777');
 
 app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.sendFile(join(process.cwd(), '/src/page.html'));
+    res.render('page')
 });
 
 app.get('/fetch', (req, res) => {
-    res.sendFile(join(process.cwd(), '/src/fetch.html'));
+    res.render('fetch')
 });
 
 app.get('/files', (req, res) => {
     const files = readdirSync(uploadDir);
     res.json({ files });
 });
+
+import popeText from "./popetext.json";
+
+app.get('/pope' , (req, res, next) => {
+    try { 
+        const text = popeText[Math.floor(Math.random() * popeText.length)]
+        throw new PopeCodeError(text)
+    } catch (err) {
+        next(err)
+    }
+})
 
 app.get('/file/:fileName', (req, res) => {
     const fileName = req.params.fileName;
@@ -43,7 +65,7 @@ app.get('/file/:fileName', (req, res) => {
 
 const upload = async (req:Request, res: Response) => {
     try {
-        const form = new IncomingForm();
+        const form = new IncomingForm({allowEmptyFiles: true});
       
         form.parse(req, (err, field, file) => {
             if (err) throw err;
@@ -64,6 +86,26 @@ const upload = async (req:Request, res: Response) => {
 app.post('/upload', upload);
 
 
+
+app.get('*', (req, res) => { //404
+    res.status(404).send("Error 404 : Page not found")
+})
+
+class PopeCodeError extends Error {
+    constructor(message: string){
+        super(message);
+        this.name = 'PopeErrorCode',
+        Object.setPrototypeOf(this, PopeCodeError.prototype)
+    }
+}
+
+app.use((err, req, res, next) => {
+    if(err instanceof PopeCodeError) {
+        res.status(410).send(`Error ${POPECODE} : ${err.message}`)
+    }
+    console.error(err)
+    res.status(500).send("Error 500 : Server side error \n"+err)
+})
 
 httpServer.listen(process.env.PORT || 3000, () => {
     console.log(`Server is listening on http://localhost:${process.env.PORT || 3000}`);
